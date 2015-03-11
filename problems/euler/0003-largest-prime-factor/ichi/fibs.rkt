419f73d42d1567e67e1f6aeb06b00b71c7aae14d
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-11 02:25:18 -0700

    first solution to largest prime factor problem

---
+++ problems/euler/0003-largest-prime-factor/ichi/fibs.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require rackunit rackunit/text-ui)
+(require math/number-theory)
+
+(define (largest-prime-factor number)
+  (apply max (map car (factorize number))))
+
+(define file-tests
+  (test-suite
+  "Test for largest prime factor"
+    (check-equal? 6857 (largest-prime-factor 600851475143))))
+
+(run-tests file-tests)
