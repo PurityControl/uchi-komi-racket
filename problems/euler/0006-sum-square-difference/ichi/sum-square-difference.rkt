96d842e560134cb55b7bb7ca6528e47c565c4c7f
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-17 00:58:08 -0700

    outline methods with failing tests

---
+++ problems/euler/0006-sum-square-difference/ichi/sum-square-difference.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require rackunit rackunit/text-ui)
+
+(define (square-sums lonumbers)
+  "square the sum of the list"
+  )
+
+(define (sum-squares lonumbers)
+  "sum the squares of the list"
+  )
+
+(define (sum-square-difference lonumbers)
+  "difference between square of sums of the the list and sum the squares of the list"
+  )
+
+
+(define file-tests
+  (test-suite
+   "Tests for sum square difference"
+   (check-equal? 385 (sum-squares (sequence->list (in-range 1 11))))
+   (check-equal? 3025 (square-sums (sequence->list (in-range 1 11))))
+   (check-equal? 2640 (sum-square-difference (sequence->list (in-range 1 11))))
+   (check-equal? 25164150 (sum-square-difference (sequence->list (in-range 1 101))))))
+
+
+(run-tests file-tests)
