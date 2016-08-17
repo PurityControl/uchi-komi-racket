9e3d1836820d676671489c7606e0f623536a12f4
craig ferry <craig@craigs-MacBook.home>
2016-08-17 21:47:34 +0100

    First iteration of counting sundays problem

---
+++ problems/euler/0019-counting-sundays/ichi/counting-sundays.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define days-in-months
+  ; january 1901 has a 2 extra days from the last Sunday
+  (lambda (year)
+    (cond
+      [(= year 1901) '(33 28 31 30 31 30 31 31 30 31 30 31)]
+      [(leap? year)  '(31 29 31 30 31 30 31 31 30 31 30 31)]
+      [ else         '(31 28 31 30 31 30 31 31 30 31 30 31)])))
+
+
+(define (leap? year)
+  (if (zero? (modulo year 4))
+      (or (zero? (modulo year 400))
+          (not (zero? (modulo year 100))))
+      false))
+
+(define all-days
+  (apply append
+         (map days-in-months
+              (sequence->list (in-range 1901 2001)))))
+
+(define cumulative-days
+  (lambda (lod total)
+    (cond
+      [(null? lod) '()]
+      [else
+       (cons (+ (car lod) total)
+             (cumulative-days (cdr lod) (+ total (car lod))))])))
+
+
+
+(count (lambda (l) (zero? (modulo l 7)))(cumulative-days all-days 0))
